ff3593df7a591960025abf8962ef814ed3062720
Ramkumar <ramsksra@gmail.com>
2025-01-30 11:41:20 +0530

    added-express-module

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const http = require("http");
 require("dotenv").config();
 const PORT = process.env.PORT;
+const express = require("express");
 
-const app = http.createServer((req, res) => {
-    res.end("Hello from server");
+const app = express();
+app.use(express.json());
+
+// Test Route added for checking purpose
+app.use("/test", (req, res, next) => {
+    res.send("Hello from server");
 });
 
 app.listen(PORT, () => {
     console.log("Applicaation is running in port ", PORT);
-})
+});
