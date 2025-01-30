088e89743156a9ea140d9cd4d1d95ca75c92af70
Ramkumar <ramsksra@gmail.com>
2025-01-30 13:25:51 +0530

    bcrypyt-library-added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const authRouter = express.Router();
 const User = require("../models/UserModel");
+const bcryptjs = require("bcryptjs");
 
 
 authRouter.post("/signup", async (req, res, next) => {
     try {
         const userData = req?.body;
         const { firstName, lastName, emailId, password } = userData;
+        const hashedPassword = await bcryptjs.hash(password.trim(), 10);
         const newUser = await User.create({
             firstName,
             lastName,
             emailId,
-            password
+            password: hashedPassword
         });
         res.status(200).json({
             message: "User signup Successfull",
